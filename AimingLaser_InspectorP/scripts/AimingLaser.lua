e3cbd44900c2a5ebde274965f79d3732ae8b4466
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:37:31 +0200

    'Update'

--- AimingLaser_InspectorP/scripts/AimingLaser.lua
+++ AimingLaser_InspectorP/scripts/AimingLaser.lua
@@ -1,20 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name:
-  AimingLaser_InspectorP
-
-  Summary:
-  Using the aiming laser on InspectorP.
-
-  How to Run:
-  A connected InpspectorP6xx device is necessary to run this sample. Starting this
-  sample is possible either by running the app (F5) or debugging (F7+F10)
-  The aiming laser on the InspectorP device lights up for five seconds.
-
-  More Information:
-  See the tutorial "Audio-visual Feedback InspectorP".
-
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
 
